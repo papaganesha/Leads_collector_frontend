8ae841b07fd75be8a866e08478b70ac90ee1614e
Joao Pedro Alves <nobzao2011@gmail.com>
2026-09-11 16:11:10 -0300

    Change import file extensions from .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import SkeletonLoader from './components/SkeletonLoader.js';
-import LeadTable from './components/LeadTable.js';
-import SavedLeads from './components/SavedLeads.js';
+import SkeletonLoader from './components/SkeletonLoader.jsx';
+import LeadTable from './components/LeadTable.jsx';
+import SavedLeads from './components/SavedLeads.jsx';
 import { supabase } from './libs/supabase.js';
 
 const NICHES = [
